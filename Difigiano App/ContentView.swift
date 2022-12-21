d028e1db717dabe54e6a845cb56f6463fb0c4b1e
Benedict Bode <benedict.bode11@gmail.com>
2022-12-21 11:54:57 +0100

    Initial Commit

---
+++ Difigiano App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Difigiano App
+//
+//  Created by Benedict Bode Privat on 21.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Difigiano App/Difigiano_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Difigiano_AppApp.swift
+//  Difigiano App
+//
+//  Created by Benedict Bode Privat on 21.12.22.
+//
+
+import SwiftUI
+
+@main
+struct Difigiano_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
